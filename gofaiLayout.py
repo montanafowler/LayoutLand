2d133cc086826457ff4d3b508976bb20951c2d47
montanafowler@yahoo.com <montanafowler@yahoo.com>
2020-03-18 16:35:34 -0700

    tuning histagram similarity parameters

--- gofaiLayout.py
+++ gofaiLayout.py
@@ -17,6 +17,38 @@ def getImageIds(directory):
         imageIds.append(processIdFromImageName(filename))
     return imageIds
 
+def appColorsAreSimilar(colorDict0, colorDict1):
+    for key0 in colorDict0.keys():
+        for key1 in colorDict1.keys():
+            color0 = key0.split(',')
+            color1 = key1.split(',')
+            colorDifference = [int(color0[0]) - int(color1[0]), int(color0[1]) - int(color1[1]), int(color0[2]) - int(color1[2])]
+            # if all three channels are withing +- 15 of each other
+            if abs(colorDifference[0]) < 15 and abs(colorDifference[1]) < 15 and abs(colorDifference[2]) < 15:
+                if abs(colorDict0[key0] - colorDict1[key1]) < 28:
+                    return True
+    return False
+
+
+def cleanUpHistagramColorDictionary(histagramColors):
+    entriesToDelete = []
+    for key in histagramColors.keys():
+        if histagramColors[key] < 90:
+            entriesToDelete.append(key)
+    for entry in entriesToDelete:
+        histagramColors.pop(entry)
+    print("histagramColors cleaned up: " + str(histagramColors))
+    return histagramColors
+
+def fillHistagramColorDictionary(cols, histagram):
+    histagramColors = {}
+    # fill histagram colors with all the colors
+    for j in range(cols):
+        colorKey = str(histagram[0,j][0]) + "," + str(histagram[0,j][1]) + "," + str(histagram[0,j][2])
+        if (colorKey in histagramColors.keys()) == False:
+            histagramColors[colorKey] = 0
+        histagramColors[colorKey] += 1
+    return histagramColors
 
 def processHistagrams(directory):
     histagramDir = directory + "//histagrams"
@@ -26,23 +58,33 @@ def processHistagrams(directory):
     histagramDict = {}
     for filename in os.listdir(histagramDir):
         histagram = cv2.imread(histagramDir + "//" + filename, cv2.IMREAD_COLOR)
+        histagram = cv2.cvtColor(histagram, cv2.COLOR_BGR2RGB)
         cols = histagram.shape[1]
-        print("shape: " + str(histagram.shape))
 
-        img = cv2.cvtColor(histagram, cv2.COLOR_BGR2RGB)
-        print("img cvt color: " + str(img[0,0]))
-        colorKey = str(img[0,0][0]) + "," + str(img[0,0][1]) + "," + str(img[0,0][2])
-        print("color_key " + str(colorKey))
-        histagramColors = {}
-        for j in range(cols):
-            colorKey = str(img[0,j][0]) + "," + str(img[0,j][1]) + "," + str(img[0,j][2])
-            if (colorKey in histagramColors.keys()) == False:
-                histagramColors[colorKey] = 0
-            histagramColors[colorKey] += 1
-        print("histagramColors: " + str(histagramColors))
+        # fill histagram colors with all the colors
+        histagramColors = fillHistagramColorDictionary(cols, histagram)
 
+        # delete the entries that don't dominate
+        histagramColors = cleanUpHistagramColorDictionary(histagramColors)
 
+        # add the dictionary to the full collection
+        histagramDict[processIdFromImageName(filename)] = histagramColors
 
+    print("histagramDict " + str(histagramDict))
+    count = 0
+    total = 0
+    for k0 in histagramDict.keys():
+        for k1 in histagramDict.keys():
+            similarity = appColorsAreSimilar(histagramDict[k0], histagramDict[k1])
+            if k0 != k1:
+                total += 1
+            if(similarity):
+                if k0 != k1:
+                    count += 1
+                    print(k0 + " and " + k1 + " are similar: " + str(similarity))
+    print ("percentage similar: " + str(float(count) / float(total)))
+    print(count/2)
+    print(total/2)
 
 def isEmpty(stack):
     return len(stack) == 0
